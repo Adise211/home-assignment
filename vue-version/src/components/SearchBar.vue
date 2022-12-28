cc961195b04cacbf9050bdedae80a182fb068e24
Adise211 <101215614+Adise211@users.noreply.github.com>
2022-12-28 09:08:36 +0200

    select inputs and json files as data

--- vue-version/src/components/SearchBar.vue
+++ vue-version/src/components/SearchBar.vue
@@ -1,13 +1,34 @@
 <template>
-    <header>
-        <h1>Search Bar</h1>
-    </header>
+    <div>
+        <div>
+            <label for="area">Area:</label>
+            <select id="area" @change="handleArea">
+                <option value="testing">testing</option>
+                <option value="testing2">testing 2</option>
+            </select>
+            <label for="city">City:</label>
+            <select id="city" @change="handleCity">
+                <option>hi</option>
+            </select>
+        </div>
+    </div>
 </template>
 
 
 <script>
 export default {
-    name: 'SearchBar'
+    name: 'SearchBar',
+    data() {
+        return {
+            areas: [],
+            cities: []
+        }
+    },
+    methods: {
+        handleArea(e) {
+            console.log("test value",e.target.value);
+        }
+    }
 }
 </script>
 
