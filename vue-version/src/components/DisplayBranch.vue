7637433a5b6fc15b4756a581772704d56516b157
Adise211 <101215614+Adise211@users.noreply.github.com>
2022-12-28 11:30:16 +0200

    display branch info

---
+++ vue-version/src/components/DisplayBranch.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div>
+            <h1>{{ branch.branch + " - " + currentCity.cityName}}</h1>
+            <p>address: {{ branch.address }}</p>
+            <p>phone: {{ branch.phone }}</p>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DisplayBranch",
+    props: ["branch", "currentCity"] 
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- vue-version/src/components/SearchBar.vue
+++ vue-version/src/components/SearchBar.vue
@@ -3,31 +3,84 @@
         <div>
             <label for="area">Area:</label>
             <select id="area" @change="handleArea">
-                <option value="testing">testing</option>
-                <option value="testing2">testing 2</option>
+                <option></option>
+                <option 
+                    :value= "ar.id"
+                    v-for="ar in areas"
+                    :key="ar.id"
+                >
+                    {{ ar.name }}
+                </option>
             </select>
             <label for="city">City:</label>
             <select id="city" @change="handleCity">
-                <option>hi</option>
+                <option>choose city</option>
+                <option
+                    :value="city.id"
+                    v-for="city in selectableCities"
+                    :key="city.id"
+                >
+                    {{ city.cityName }}
+                </option>
             </select>
         </div>
+        <button @click="handleSearch">Search</button>
+        <DisplayBranch v-bind:branch="branch" v-bind:currentCity="currentCity" v-if="display === true"/>
     </div>
 </template>
 
 
 <script>
+import areasJson from '../data/areas.json';
+import citiesJson from '../data/cities.json';
+import branchesJson from '../data/branches.json';
+import DisplayBranch from './DisplayBranch.vue';
+
 export default {
     name: 'SearchBar',
     data() {
         return {
             areas: [],
-            cities: []
+            cities: [],
+            branchesList: [],
+            selectableCities: [],
+            selectedCityId: '',
+            display: false,
+            branch: [],
+            currentCity: []
+
         }
     },
     methods: {
         handleArea(e) {
-            console.log("test value",e.target.value);
+            const { value } = e.target;
+            const findCities = this.cities.filter((city) => {
+            if(city.area === value) return city
+        });
+            this.selectableCities = findCities;
+        },
+        handleCity(e) {
+            const { value } = e.target;
+            this.selectedCityId = value;
+        },
+        handleSearch() {
+            const findBranch = this.branchesList.filter((x) => {
+                return x.cityId === this.selectedCityId
+            });
+            const find = this.selectableCities.filter( (x) => { return x.id === findBranch[0].cityId});
+            this.currentCity = find[0];
+            this.display = true;
+            this.branch = findBranch[0];
+
         }
+    },
+    created() {
+        this.areas = areasJson;
+        this.cities = citiesJson;
+        this.branchesList = branchesJson;
+    },
+    components: {
+        DisplayBranch
     }
 }
 </script>
