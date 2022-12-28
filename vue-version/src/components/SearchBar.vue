719630db770f311a39119423c82dda3701ad64e4
Adise211 <101215614+Adise211@users.noreply.github.com>
2022-12-28 12:10:26 +0200

    search bar design

--- vue-version/src/components/SearchBar.vue
+++ vue-version/src/components/SearchBar.vue
@@ -1,30 +1,32 @@
 <template>
     <div>
-        <div>
-            <label for="area">Area:</label>
-            <select id="area" @change="handleArea">
-                <option></option>
-                <option 
-                    :value= "ar.id"
-                    v-for="ar in areas"
-                    :key="ar.id"
-                >
-                    {{ ar.name }}
-                </option>
-            </select>
-            <label for="city">City:</label>
-            <select id="city" @change="handleCity">
-                <option>choose city</option>
-                <option
-                    :value="city.id"
-                    v-for="city in selectableCities"
-                    :key="city.id"
-                >
-                    {{ city.cityName }}
-                </option>
-            </select>
+        <div class='s-container'>
+            <div class="inputs">
+                <label for="area">Area:</label>
+                <select id="area" @change="handleArea" class="select-1">
+                    <option></option>
+                    <option 
+                        :value= "ar.id"
+                        v-for="ar in areas"
+                        :key="ar.id"
+                    >
+                        {{ ar.name }}
+                    </option>
+                </select>
+                <label for="city">City:</label>
+                <select id="city" @change="handleCity" class="select-2">
+                    <option>choose city</option>
+                    <option
+                        :value="city.id"
+                        v-for="city in selectableCities"
+                        :key="city.id"
+                    >
+                        {{ city.cityName }}
+                    </option>
+                </select>
+            </div>
+            <button @click="handleSearch" :disabled='selectableCities.length === 0'>Search</button>
         </div>
-        <button @click="handleSearch">Search</button>
         <DisplayBranch v-bind:branch="branch" v-bind:currentCity="currentCity" v-if="display === true"/>
     </div>
 </template>
@@ -86,7 +88,43 @@ export default {
 </script>
 
 <style scoped>
-    h1 {
-       color: blueviolet; 
+    .s-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        border: 1px solid ghostwhite;
+        background-color: ghostwhite;
+        box-shadow: 3px 3px 3px 3px grey;
+        padding: 100px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 50px;
+        width: 70em;
     }
+    .select-1, .select-2 {
+        padding: 10px;
+        width: 150px; 
+        font-size: 16px;
+    }
+    .select-1 {
+       margin-right: 80px;
+    }
+    label {
+        font-weight: bold;
+        margin-right: 1em;
+    }
+    button {
+        background-color: hotpink;
+        margin-left: 8em;
+        padding: 1em;
+        width: 10em;
+        border: 1px solid hotpink;
+        border-radius: 5em;
+        box-shadow: 2px 2px 2px black;
+        cursor: pointer;
+    }
+    button:active {
+        background-color: #ff87c3;
+    }
+
 </style>
