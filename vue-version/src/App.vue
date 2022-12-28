858c5f4f21870520578e55275c16165ab8803c6b
Adise211 <101215614+Adise211@users.noreply.github.com>
2022-12-28 08:50:35 +0200

    created search bar

--- vue-version/src/App.vue
+++ vue-version/src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <SearchBar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBar
   }
 }
 </script>
---
+++ vue-version/src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <h1>Search Bar</h1>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: 'SearchBar'
+}
+</script>
+
+<style scoped>
+    h1 {
+       color: blueviolet; 
+    }
+</style>
