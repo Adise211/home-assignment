08fd4379f3b9d467912a8b1e231288c5b82f9478
Adise <adisemamo211@gmail.com>
2025-10-28 14:47:13 -0400

    feat: add Redux for state management with counter functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
-// Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -21,7 +21,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StrictMode>
   );
 }
